73db1f7310cf8ca308c083f0402ab27770456e3a
Tunkert <tunkert@gmail.com>
2022-11-22 07:22:15 -0700

    lesson on escape characters

---
+++ 002_escape_characters.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	printf("\tThis string is tabbled over.\n");
+	printf("This string has \"double quotes\" in it.\n");
+	printf("This string has \'single quotes\' in it.\n");
+	return 0;
+}
